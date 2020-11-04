87c783218b3f16e7d3a191a8cd1edd1974d1eb9d
Nilanshu V Rajmane <rajmanenilanshu@gmail.com>
2020-11-04 14:50:03 +0530

    Fixed syntax warnings

--- client.c
+++ client.c
@@ -13,8 +13,8 @@ int main()
 	proxy = min_min_bus_gdbus_proxy_new_for_bus_sync(G_BUS_TYPE_SESSION, G_DBUS_PROXY_FLAGS_NONE,
 			"com.fatminmin", "/com/fatminmin/GDBUS", NULL, &error);
 
-	min_min_bus_gdbus_call_hello_world_sync(proxy, "fatminmin", &buf, NULL, &error);
-	g_print("resp: %s\n", buf);
+	min_min_bus_gdbus_call_hello_world_sync(proxy, "Nilanshu", buf, NULL, &error);
+	g_print("resp: %s\n", *buf);
 
 	g_object_unref(proxy);
 	return 0;
